7554a4e2b69b98df6c32cb9dcb6b26a230dc3e08
TheRufio <therufio228@gmail.com>
2025-08-26 18:54:02 +0300

    Try to extend idea of project

--- catmate/gallery/models.py
+++ catmate/gallery/models.py
@@ -15,8 +15,42 @@ from django.db import models
     # day/season/game/ivent/gallery/chat publication and so on.. think about it and carrency (for achievements)
     gallery (user1<-gallery->user2) optimization!!!!
 
+    ~ Registration ~
+        CustomUser:
+            username
+            firstname
+            lastname
+            age
+            gender
+        Profile:
+            chats
+            special chats
+            galleries
+            interests
+
+    ~ Gallery ~
+        Gallery:
+            image
+            description
+            relation
+            date
+            is_special_day   
+    ~ Chat ~
+        Chat:
+
+
+    ~ Relation ~
+
+    ~ Market ~
+
+
     # love and friend coin
     # recomendation system that mean:
     # Profile (user): interests like football, sport, programming...
-    
+    #
+    # Applications: ~ registration  (username, password, email, gender, interests)
+    #               ~ gallery       (photoes folder, special days...)
+    #               + chat          (chats, speacial chat *with couple or friend after "CatLink"*)
+    #               + relation      (friends, couple, achievements, love & friends coin)
+    #               + market        (buy & sale: gifts, unique features... )
 """
